82a64fcfc3d54869b264262d7c2dce06e7ef02e1
cgallardo2 <cgallardo@canto.com>
2022-02-04 11:53:57 -0800

    tester

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/")
 public class HomeController {
     @GetMapping
 public String message(){
---
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerTest {
+    @Test
+    void contextLoads(){
+
+    }
+}
